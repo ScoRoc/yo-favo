a11931d6f6bbd124f9ea0d7a2c8bbfbfcad99925
Scott <scrosehart@gmail.com>
2018-02-28 16:10:35 -0800

    working on sortable order staying in order

--- public/js/script.js
+++ public/js/script.js
@@ -1,6 +1,24 @@
 console.log('script.js is working');
 
+var updateOrder = function(event, ui){
+    console.log("New sort order!");
+};
+
 $(function() {
-  $('#sortable').sortable();
+  $('#sortable').sortable({
+    update: updateOrder
+  });
   $('#sortable').disableSelection();
 });
+
+var listElements = $('#sortable').children();
+
+console.log(listElements);
+
+var listValues = [];
+
+listElement.forEach(function(element) {
+    listValues.push(element.innerHTML);
+});
+
+console.log(listValues);
