7e02bbf8124d0ffb84f318166c81866b9485a49f
Scott <scrosehart@gmail.com>
2018-03-02 17:27:32 -0800

    deleted duplicate migrations file for favos_users and added order column to the one im keeping

--- migrations/20180228013053-create-favos-users.js
+++ migrations/20180228013053-create-favos-users.js
@@ -14,6 +14,9 @@ module.exports = {
       favoId: {
         type: Sequelize.INTEGER
       },
+      order: {
+        type: Sequelize.INTEGER
+      },
       createdAt: {
         allowNull: false,
         type: Sequelize.DATE
